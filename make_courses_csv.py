deced2cc1765da8ac52f99fdd5fdde663cdfb5f8
Vasily Ilin <vasilin97@gmail.com>
2025-09-01 13:35:19 -0700

    add course embeddings to csv

--- make_courses_csv.py
+++ make_courses_csv.py
@@ -1,6 +1,8 @@
 """Create a CSV file with unique courses and their topics from the project_subjects.csv file."""
 #%%
 import pandas as pd
+import numpy as np
+import json
 
 courses_df = pd.read_csv(
     'data/download/project_subjects.csv',
@@ -14,7 +16,16 @@ agg_dict['subject_short_name'] = lambda x: ', '.join(x.dropna().astype(str).uniq
 courses_df = courses_df.groupby('project_id', as_index=False).agg(agg_dict)
 courses_df = courses_df.rename(columns={'subject_short_name': 'topics', 'fname': 'university'})
 courses_df = courses_df.drop(columns=['subject_name', 'subject_id', 'parent_subject_id', 'fcode', 'l_key', 'r_key', 'level', 'subject_page']).reset_index(drop=True)
+courses_df
 
-courses_df.to_csv('data/generated/courses.csv', index=False)
 #%%
-courses_df['subject_short_name'][0]
+# add embeddings column
+with np.load("course_embeddings/course_embeddings.npz") as data:
+    loaded_ids = data["ids"].astype(int)
+    embeddings = data["embeddings"].astype(float)
+embeddings = embeddings.astype(np.float32)
+
+courses_df['embedding'] = [json.dumps(vec.tolist()) for vec in embeddings]
+courses_df.to_csv('data/generated/courses.csv', index=False)
+
+# %%
